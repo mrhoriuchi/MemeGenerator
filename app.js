b61e1e7a28308b119da375d332e4150fc1c5ee5b
Kody Horiuchi <111256751+mrhoriuchi@users.noreply.github.com>
2022-10-26 18:36:13 -0400

    Readability
    This didn't change how the code functioned. But now it looks cleaner and more readable as you know what each part is doing.

--- app.js
+++ app.js
@@ -4,25 +4,35 @@ const topTextInput = document.querySelector('input[name="topText"]');
 const bottomTextInput = document.querySelector('input[name="bottomText"]');
 const resultSection = document.querySelector('#results');
 
+function memeMaker(){
+    let imageUrl = urlInput.value;
+    const meme = document.createElement('div');
+    meme.style.backgroundImage = `url(${imageUrl})`;
+    return meme;
+}
+
+function topWriting(){
+    const top = document.createElement('h2');
+    top.setAttribute('class', 'topInput');
+    top.innerText = topTextInput.value;
+    return top;
+}
+
+function bottomWriting(){
+    const bottom = document.createElement('h2');
+    bottom.setAttribute('class', 'bottomInput');
+    bottom.innerText = bottomTextInput.value;
+    return bottom;
+}
 
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
-    let imageUrl = urlInput.value;
-    const newMeme = document.createElement('div');
-    const newTop = document.createElement('h2');
-    const newBottom = document.createElement('h2');
-
-    newTop.setAttribute('class', 'topInput');
-    newBottom.setAttribute('class', 'bottomInput');
-
-    newMeme.style.backgroundImage = `url(${imageUrl})`;
-
-    newTop.innerText = topTextInput.value;
-    newBottom.innerText = bottomTextInput.value;
+    const newMeme = memeMaker();
+    const newTop = topWriting();
+    const newBottom = bottomWriting();
 
     newMeme.appendChild(newTop);
     newMeme.appendChild(newBottom);
-
     resultSection.appendChild(newMeme);
     
     urlInput.value = '';
@@ -33,8 +43,7 @@ form.addEventListener('submit', function(e) {
 resultSection.addEventListener('click', function(e){
     if(e.target.tagName === 'DIV'){
         e.target.remove()
-    }
-    else if(e.target.tagName === 'H2'){
+    } else if(e.target.tagName === 'H2'){
         e.target.parentElement.remove();
     }
 });
